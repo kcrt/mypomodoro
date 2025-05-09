e204691e984da7c3921a99c00973a911c50a1b9a
TAKAHASHI, Kyohei <kcrt@kcrt.net>
2025-05-09 22:07:43 +0900

    :lipstick: Window size and position change

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use std::f32::consts::TAU;
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([400.0, 400.0]),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([265.0, 380.0])
+            .with_resizable(false)
+            .with_always_on_top(),
         ..Default::default()
     };
     eframe::run_native(
-        "My pomodoro",
+        "My Pomodoro",
         options,
         Box::new(|_cc| {
             Ok(Box::<MyApp>::default())
